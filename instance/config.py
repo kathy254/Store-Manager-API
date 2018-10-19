7b556356b4a850e8edbb2888ecd6189b1b52a7f2
kathy254 <catherine.omondi2@gmail.com>
2018-10-19 09:35:37 +0300

    made changes to config file

--- instance/config.py
+++ instance/config.py
@@ -1,13 +1,40 @@
-class DevelopmentConfig:
-    DEBUG = True
-    ENV = 'development'
+import os
 
 
-class TestingConfig:
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('FLASK_SECRET')
+    
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
     DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@localhost:3306/test_WSCartAPI' % (os.getenv('MYSQL_USER'),
+                                                                          os.getenv('MYSQL_PASSWORD'))
+    DEBUG = True
+
+
+class StagingConfig(Config):
+    """Configurations for Staging."""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
 
 
 app_config = {
+    'development': DevelopmentConfig,
     'testing': TestingConfig,
-    'development': DevelopmentConfig
+    'staging': StagingConfig,
+    'production': ProductionConfig,
 }
