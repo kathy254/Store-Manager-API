6935bb4c47df09c5b4f29497794dc2cf35e8b115
kathy254 <catherine.omondi2@gmail.com>
2018-10-19 07:30:27 +0300

    updates sales.py

--- app/api/v1/model/sales.py
+++ app/api/v1/model/sales.py
@@ -8,10 +8,10 @@ class Sales(Verify):
 
 	def check_user_input(self):
 		if self.is_sales_payload(self.items) is False:
-			return {'result': 'invalid payload'},406
+			return {'result': 'Payload is invalid'},406
 		elif self.items['quantity'] < 1:
-			return {'result': 'quantity can not be less than one'},406
+			return {'result': 'Product quantity can not be less than 1'},406
 		elif self.items['productId'] < 0:
-			return {'result': 'productId can not be less than zero'},406
+			return {'result': 'productId can not be less than 0'},406
 		else:
 			return 1
