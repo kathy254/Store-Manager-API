1ab9f86ab855f44f1ef71e36137a81c9cb32b35c
kathy254 <43727540+kathy254@users.noreply.github.com>
2018-10-19 09:42:38 +0300

    Update test_verfy.py

--- app/test/v1/test_verfy.py
+++ app/test/v1/test_verfy.py
@@ -5,35 +5,33 @@ from app.api.v1.model.verify import Verify
 
 
 class TestVerify(unittest.TestCase):
-    def setUp(self):
-        self.obj = Verify()
-
-
-    def tearDown(self):
-        self.obj = None
-
-
-    def test_is_empty(self):
-        test = self.obj.is_empty(['', 'dress'])
-        self.assertTrue(test)
-
-
-    def test_is_not_empty(self):
-        test = self.obj.is_empty(['suit', 'suit'])
-        self.assertFalse(test)
-
-
-    def test_whitespace(self):
-        test = self.obj.is_whitespace([' ', 'm'])
-        self.assertTrue(test)
-
-
-    def test_no_whitespace(self):
-        test = self.obj.is_whitespace(['k', 'lm'])
-        self.assertFalse(test)
-
-
-    def test_not_payload(self):
+	def setUp(self):
+		self.obj = Verify()
+	
+	
+	def tearDown(self):
+		self.obj = None
+		
+		
+	def test_is_empty(self):
+		test = self.obj.is_empty(['', 'dress'])
+		self.assertTrue(test)
+		def test_is_not_empty(self):
+			test = self.obj.is_empty(['suit', 'suit'])
+			self.assertFalse(test)
+			
+	
+	def test_whitespace(self):
+		test = self.obj.is_whitespace([' ', 'm'])
+		self.assertTrue(test)
+		
+	
+	def test_no_whitespace(self):
+		test = self.obj.is_whitespace(['k', 'lm'])
+		self.assertFalse(test)
+		
+		
+	def test_not_payload(self):
 		payload = {'name':'Gucci dress','moq':3,'quantity':0,'qwr':0}
 		payload_2 = {'name':'Gucci dress','moq':3}
 		payload_3 = {'name':'Gucci dress','moq':3,'quantity':0,'category':0,'we':0}
