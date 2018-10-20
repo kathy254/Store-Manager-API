121b4dad1be88a7f9c27dd16c5c672ad5945a657
kathy254 <catherine.omondi2@gmail.com>
2018-10-20 14:09:36 +0300

    [Feature #16136168] Made changes to init file to enable authorizations

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,11 +4,21 @@ from flask_restplus import Api
 
 from .views.products import store_product
 from .views.sales import store_sales
+from .views.users import store_users
+
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
 
 
 app_v1 = Blueprint('app_v1',__name__)
-api_v1 = Api(app_v1, title='Store Manager', version='1.0', description='Store Manager api v1')
+api_v1 = Api(app_v1, title='Store Manager', version='1.0', description='Store Manager api v1', authorizations=authorizations)
 
 
 api_v1.add_namespace(store_product)
 api_v1.add_namespace(store_sales)
+api_v1.add_namespace(store_users)
